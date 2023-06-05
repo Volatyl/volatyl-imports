3b5910823cd99e7c98bc75cdede06ead1b23bc0c
Kennedy Muinde <kennedymuinde@gmail.com>
2023-06-05 20:09:06 +0300

    Added alembic migrations

--- endpoints.py
+++ endpoints.py
@@ -1,20 +1,127 @@
 from fastapi import FastAPI
-from models import Car, Brand, engine
 from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+from models import Car, engine, User, Review, user_car
 
 app = FastAPI()
 
+# pydantic classes for cars
+
+
+class Cartype(BaseModel):
+    brand: str
+    model: str
+    year: int
+    mileage: int
+    price: int
+    cc: int
+    description: str
+
+
 Session = sessionmaker(bind=engine)
-session = Session()
+
+# code to handle cars
 
 
-@app.get('/', tags=['home'])
-def get_data():
-    with session as s:
-        cars = s.query(Car).all()
+@app.get('/cars', tags=['Cars'])
+def get_cars():
+    session = Session()
+    cars = session.query(Car).all()
+    session.close()
     return cars
 
-@app.post('/')
-def add_brand():
-    with session as s:
-        brand
+
+@app.post('/cars', tags=['Cars'])
+def add_car(car: Cartype):
+    session = Session()
+    car1 = Car(brand=car.brand, model=car.model, year=car.year, mileage=car.mileage,
+               price=car.price, cc=car.cc, description=car.description)
+    session.add(car1)
+    session.commit()
+    session.close()
+    return 'Car added successfully'
+
+
+@app.put('/cars/{id}', tags=['Cars'])
+def edit_car(id: int, car: Cartype):
+    session = Session()
+    car1 = session.query(Car).filter_by(id=id).first()
+    if car1:
+        if car.brand:
+            car1.brand = car.brand
+        if car.model:
+            car1.model = car.model
+        if car.year:
+            car1.year = car.year
+        if car.mileage:
+            car1.mileage = car.mileage
+        if car.price:
+            car1.price = car.price
+        if car.cc:
+            car1.cc = car.cc
+        if car.description:
+            car1.description = car.description
+
+        session.commit()
+        session.close()
+        return 'Car updated successfully'
+    else:
+        session.close()
+        return 'Car not found'
+
+
+@app.delete('/cars/{id}', tags=['Cars'])
+def delete_car(id: int):
+    session = Session()
+    session.query(Car).filter_by(id=id).delete()
+    session.commit()
+    session.close()
+    return 'Car deleted successfully'
+
+
+# pydantic class for user
+class Usertype(BaseModel):
+    name: str
+    email: str
+
+
+# code to handle users
+@app.get('/users', tags=['Users'])
+def get_users():
+    session = Session()
+    users = session.query(User).all()
+    return users
+
+
+@app.post('/users/{id}', tags=['Users'])
+def add_user(user: Usertype):
+    session = Session()
+    user1 = User(name=user.name, email=user.email)
+    session.add(user1)
+    session.commit()
+    session.close()
+    return 'User added successfully'
+
+
+# pydantic class for reviews
+class Reviewtype(BaseModel):
+    name: str
+    email: str
+
+
+# code to handle reviews
+@app.get('/users', tags=['Users'])
+def get_users():
+    session = Session()
+    users = session.query(User).all()
+    return users
+
+
+@app.post('/users/{id}', tags=['Users'])
+def add_user(user: Usertype):
+    session = Session()
+    user1 = User(name=user.name, email=user.email)
+    session.add(user1)
+    session.commit()
+    session.close()
+    return 'User added successfully'
--- models.py
+++ models.py
@@ -34,6 +34,9 @@ class Review(Base):
     id = Column(Integer(), primary_key=True)
     review = Column(String())
 
+    user_id = Column(Integer(), ForeignKey('users.id'))
+    car_id = Column(Integer(), ForeignKey('cars.id'))
+
     cars = relationship('Car', back_populates='review')
     user = relationship('User', back_populates='review')
 
@@ -45,6 +48,7 @@ class Car(Base):
     __tablename__ = 'cars'
 
     id = Column(Integer(), primary_key=True)
+    brand = Column(String())
     model = Column(String())
     year = Column(Integer())
     mileage = Column(Integer())
@@ -53,20 +57,7 @@ class Car(Base):
     description = Column(String())
 
     review = relationship('Review', back_populates='cars')
-    brand = relationship('Brand', back_populates='cars')
     users = relationship('User', secondary='user_car', back_populates='cars')
 
     def __repr__(self):
         return f'Model: {self.model}, Year: {self.year}'
-
-
-class Brand(Base):
-    __tablename__ = 'brands'
-
-    id = Column(Integer(), primary_key=True)
-    brand = Column(String())
-
-    cars = relationship('Car', back_populates='brand')
-
-    def __repr__(self):
-        return f'Brand: {self.brand}'
