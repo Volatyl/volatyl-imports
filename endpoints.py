8802601bff7577e116cf42a33282b0d7c3fa99a1
Kennedy Muinde <kennedymuinde@gmail.com>
2023-06-05 14:37:52 +0300

    Added tables

--- endpoints.py
+++ endpoints.py
@@ -1,1 +1,20 @@
 from fastapi import FastAPI
+from models import Car, Brand, engine
+from sqlalchemy.orm import sessionmaker
+
+app = FastAPI()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+@app.get('/', tags=['home'])
+def get_data():
+    with session as s:
+        cars = s.query(Car).all()
+    return cars
+
+@app.post('/')
+def add_brand():
+    with session as s:
+        brand
--- models.py
+++ models.py
@@ -1,10 +1,44 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 engine = create_engine('sqlite:///database.db')
 
 Base.metadata.create_all(engine)
+
+
+user_car = Table(
+    'user_car', Base.metadata,
+    Column('user_id', ForeignKey('users.id')),
+    Column('car_id', ForeignKey('cars.id')),
+)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    email = Column(String())
+
+    review = relationship('Review', back_populates='user')
+    cars = relationship('Car', secondary='user_car', back_populates='users')
+
+    def __repr__(self):
+        return f'Name: {self.name}'
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    review = Column(String())
+
+    cars = relationship('Car', back_populates='review')
+    user = relationship('User', back_populates='review')
+
+    def __repr__(self):
+        return f'Review: {self.review}'
 
 
 class Car(Base):
@@ -18,7 +52,9 @@ class Car(Base):
     cc = Column(Integer())
     description = Column(String())
 
+    review = relationship('Review', back_populates='cars')
     brand = relationship('Brand', back_populates='cars')
+    users = relationship('User', secondary='user_car', back_populates='cars')
 
     def __repr__(self):
         return f'Model: {self.model}, Year: {self.year}'
@@ -30,7 +66,7 @@ class Brand(Base):
     id = Column(Integer(), primary_key=True)
     brand = Column(String())
 
-    cars = relationship('Brand', back_populates='brand')
+    cars = relationship('Car', back_populates='brand')
 
     def __repr__(self):
         return f'Brand: {self.brand}'
