bc98d176e3319396747d6d4cd496ccf26e4ac8a0
Kennedy Muinde <kennedymuinde@gmail.com>
2023-06-13 09:24:19 +0300

    new site

--- endpoints.py
+++ endpoints.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://amazing-sprite-9addd8.netlify.app/", "https://amazing-sprite-9addd8.netlify.app", "https://volatyl-imports.onrender.com/","http://localhost:5173"],
+        "https://marvelous-sopapillas-a297ed.netlify.app/", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
