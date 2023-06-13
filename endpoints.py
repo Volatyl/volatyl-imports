13ca1d87236e1539852ff561f09543fc1480bcc2
Kennedy Muinde <87446128+Volatyl@users.noreply.github.com>
2023-06-13 09:41:29 +0300

    Update endpoints.py

--- endpoints.py
+++ endpoints.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://marvelous-sopapillas-a297ed.netlify.app/", "http://localhost:5173"],
+        "https://marvelous-sopapillas-a297ed.netlify.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
